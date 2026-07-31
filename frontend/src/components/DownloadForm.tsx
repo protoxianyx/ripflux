4811541e985ab7fe99d7ceb586aca10296a9ee68
Xianyx <pushkar44320@gmail.com>
2026-08-01 05:05:14 +0530

    built the basic frontend, enough for the tesing purpose

---
+++ frontend/src/components/DownloadForm.tsx
@@ -0,0 +1,41 @@
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { Button } from "./ui/button";
+
+export default function DownloadForm() {
+    return (
+      <div className="flex items-center gap-4">
+        <Select>
+          <SelectTrigger className="w-48">
+            <SelectValue placeholder="Format" />
+          </SelectTrigger>
+
+          <SelectContent>
+            <SelectItem value="Video">Video</SelectItem>
+            <SelectItem value="Audio">Audio</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <Select>
+          <SelectTrigger className="w-48">
+            <SelectValue placeholder="Resolution" />
+          </SelectTrigger>
+
+          <SelectContent>
+            <SelectItem value="2160">2160p</SelectItem>
+            <SelectItem value="1440">1440p</SelectItem>
+            <SelectItem value="1080">1080p</SelectItem>
+            <SelectItem value="720">720p</SelectItem>
+            <SelectItem value="480">480p</SelectItem>
+          </SelectContent>
+        </Select>
+
+        <Button className="ml-auto px-8">Download</Button>
+      </div>
+    )
+}
